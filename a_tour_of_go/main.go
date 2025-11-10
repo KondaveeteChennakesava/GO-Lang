d81e66caaa141cc6e8667f6e0d9d980d96c4db68
KondaveeteChennakesava <chennakesavakondaveete@gmail.com>
2025-11-10 12:17:54 +0530

    exports in golang

--- a_tour_of_go/main.go
+++ a_tour_of_go/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"a_tour_of_go/imports"
 	"a_tour_of_go/packages"
+	"a_tour_of_go/exports"
 )
 
 // main is the single entrypoint that calls the example functions from the subpackages.
 func main() {
 	packages.PrintRandom()
 	imports.PrintSqrt()
+	exports.PrintPi()
 }
