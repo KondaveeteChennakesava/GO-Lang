b6921280d80603ba292e26750b61d7d02f2b50da
KondaveeteChennakesava <chennakesavakondaveete@gmail.com>
2025-11-10 12:32:28 +0530

    function extended in golang

--- a_tour_of_go/functions/functions.go
+++ a_tour_of_go/functions/functions.go
@@ -12,6 +12,17 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func add_continued(x, y int) int {
+	return x + y
+}
+
+func multiple_results(a, b string) (string, string) {
+	return b, a
+}
+
 func PrintAdd() {
-	fmt.Println(add(2, 5))
+	fmt.Println("Addition of 2 and 5 ",add(2, 5))
+	fmt.Println("Addition continued ", add_continued(2, 5))
+	a, b := multiple_results("Hello", "World")
+	fmt.Println("Multiple retun values ", a, b)
 }
